376aa52a68820b90a81fe0d37f8a67bf9491718e
Sruthi R <rshruti451@gmail.com>
2026-02-27 15:24:42 +0530

    added-database-url-from-neon

--- src/config/db.js
+++ src/config/db.js
@@ -3,16 +3,27 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
+// Check if we are in production/Render or local
+export const sequelize = process.env.DATABASE_URL 
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // Required for Neon/Render
+        }
+      }
+    })
+  : new Sequelize(
+      process.env.DB_NAME, 
+      process.env.DB_USER, 
+      process.env.DB_PASSWORD, 
+      {
         host: process.env.DB_HOST,
         dialect: 'postgres',
-        logging: false,
-    }
-);
+        port: process.env.DB_PORT
+      }
+    );
 
 export const connectDB = async () => {
     try {
